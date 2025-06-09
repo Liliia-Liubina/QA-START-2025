57459b902a94bb7139d166b9f5dca57eb5a6667a
HomeWor3 <lubinalilya@gmail.com>
2025-06-09 15:50:02 +0300

    HomeWork 6v1, Plane finish

---
+++ src/main/java/org/prog/Plane1.java
@@ -0,0 +1,15 @@
+package org.prog;
+
+public class Plane1 {
+
+    public int current;
+    public int max;
+    public String id;
+
+    public void destination() {
+        System.out.println("The plane " + id +
+                " is flying to New York with " + current + " number of passengers");
+    }
+
+
+}
