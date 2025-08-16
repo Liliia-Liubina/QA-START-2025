a951f4d1f4923a58e4835082851448e3e082e967
HomeWor3 <lubinalilya@gmail.com>
2025-08-16 21:55:29 +0300

    HomeWorkAuto 11, Add DataHolder11

---
+++ src/main/java/org/prog/session11/DataHolder11.java
@@ -0,0 +1,9 @@
+package org.prog.session11;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataHolder11 {
+
+    public static final Map<String,Object> MYDATA = new HashMap<String, Object>();
+}
